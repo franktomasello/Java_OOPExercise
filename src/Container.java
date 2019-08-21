01e5ead6658673de1a65fa93fc7a3d6ee13d3d7f
ftomasello <ftomasello@callutheran.edu>
2019-08-20 17:41:38 -0700

    Initial commit

---
+++ src/Container.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Container {
+
+    private final int MAX_WEIGHT_LIMIT;
+    private ArrayList<Suitcase> suitcases;
+
+    public Container(int MAX_WEIGHT_LIMIT) {
+        this.MAX_WEIGHT_LIMIT = MAX_WEIGHT_LIMIT;
+        this.suitcases = new ArrayList<>();
+    }
+
+    public void addSuitcase(Suitcase suitcase)
+    {
+        if(suitcase.totalWeight() + totalContainerWeight() <= MAX_WEIGHT_LIMIT)
+        {
+            suitcases.add(suitcase);
+        }
+    }
+
+    public int totalContainerWeight()
+    {
+        int containerWeight = 0;
+        for (Suitcase s : suitcases)
+        {
+            containerWeight += s.totalWeight();
+        }
+        return containerWeight;
+    }
+
+    public String toString()
+    {
+        if(suitcases.size() == 0)
+        {
+            return "empty" + " " + "(" + totalContainerWeight() + " " + "kg" + ")";
+        }
+        if(suitcases.size() == 1)
+        {
+            return suitcases.size() + " " + "thing" + " " + "(" + totalContainerWeight() + " " + "kg" + ")";
+        }
+        return suitcases.size() + " " + "things" + " " + "(" + totalContainerWeight() + " " + "kg" + ")";
+    }
+
+    public void printThings()
+    {
+        for (Suitcase s : suitcases)
+        {
+            s.printThings();
+        }
+    }
+
+
+}
---
+++ src/Suitcase.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class Suitcase {
+
+    private ArrayList<Thing> things;
+    private final int MAX_WEIGHT_LIMIT;
+
+    public Suitcase(int MAX_WEIGHT_LIMIT)
+    {
+        this.things = new ArrayList<>();
+        this.MAX_WEIGHT_LIMIT = MAX_WEIGHT_LIMIT;
+    }
+
+    public void addThing(Thing thing)
+    {
+        if(totalWeight() + thing.getWeight() <= MAX_WEIGHT_LIMIT) {
+            things.add(thing);
+        }
+    }
+
+    public String toString()
+    {
+        if(things.size() == 0)
+        {
+            return "empty" + " " + "(" + totalWeight() + " " + "kg" + ")";
+        }
+        if(things.size() == 1)
+        {
+            return things.size() + " " + "thing" + " " + "(" + totalWeight() + " " + "kg" + ")";
+        }
+        return things.size() + " " + "things" + " " + "(" + totalWeight() + " " + "kg" + ")";
+    }
+
+    public int totalWeight()
+    {
+        int total = 0;
+        for (Thing t : things)
+        {
+            total += t.getWeight();
+        }
+        return total;
+    }
+
+    public void printThings()
+    {
+        for(Thing t : things)
+        {
+            System.out.println(t);
+        }
+    }
+
+    public Thing heaviestThing()
+    {
+        int heaviest = 0;
+        int heaviestThingIndex = 0;
+        if(things.isEmpty())
+        {
+            return null;
+        }
+        for(int i = 0; i < things.size(); i ++)
+        {
+            if(things.get(i).getWeight() > heaviest)
+            {
+                heaviestThingIndex = i;
+                heaviest = things.get(i).getWeight();
+            }
+        }
+        return things.get(heaviestThingIndex);
+    }
+
+
+}
